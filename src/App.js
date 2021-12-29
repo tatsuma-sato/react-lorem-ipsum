7a348467de372716692350d853affc71fd044af0
tatsuma-sato <t.tsuma.s@gmail.com>
2021-12-28 17:26:21 -0800

    added features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count)
+    if(count <= 0){
+      amount = 1
+    }
+    if(count > data.length){
+      amount = data.length
+    }
+    setText(data.slice(0,amount));
+  };
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
